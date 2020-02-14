960372b31b4a438953cdf6a8a00c236ef91b083b
JamesT-35 <jamesgtatejr@gmail.com>
2020-02-14 14:28:55 -0600

    revised javascript

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
 
 
 let number = prompt("What is your balance?");
-let number2 = parseInt("You lost $10. Your new balance is:[]")
-let answer = parseInt(number) - parseInt(number2)
+let number2 = prompt("You lost $10. Your new balance is:[]")
+let total = (parseInt(number) - parseInt(number2))
+console.log(total)
 
+
+//let ask = prompt("Enter your number grade")
+//let ask2 = prompt("Enter your credit card")
+//let total = (parseInt(ask) + parseInt(ask2))
+//console.log(total)  
