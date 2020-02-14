01644a39b4fa03d34dfe984fc7c77219eb86bf0d
autumn-ragland <raglanda998@gmail.com>
2020-02-14 15:06:46 -0600

    graded

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-//let name1 = ("My name is:")
-//let name = ("James")
+let name1 = ("My name is:")
+let name = ("James")
 
-//console.log(name1 + name)
+console.log(name1 + name)
 
 
 let number = prompt("What is your balance?");
+// !! make sure your prompt aligns with the question
 let number2 = prompt("You lost $10. Your new balance is:[]")
 let total = (parseInt(number) - parseInt(number2))
 console.log(total)
 
 
-//let ask = prompt("Enter your number grade")
-//let ask2 = prompt("Enter your credit card")
-//let total = (parseInt(ask) + parseInt(ask2))
-//console.log(total)  
+// let ask = prompt("Enter your number grade")
+// // !! make sure your prompt aligns with the question
+// let ask2 = prompt("Enter your credit card")
+// let total = (parseInt(ask) + parseInt(ask2))
+// console.log(total)  
